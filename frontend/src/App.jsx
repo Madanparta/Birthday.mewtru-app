d583e3ea332caa58d95ca1bf76d4ccb9d1c79198
Madan k <partagowda15@gmail.com>
2024-08-04 17:30:23 +0530

    adding some staff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {Routes,Route} from 'react-router-dom';
 import WishesCreatingPage from './components/wishesCreatingPage'
 
 const App = () => {
   return (
     <section className='w-full px-3 py-3 md:p-10 h-screen bg-[#fbf6e234]'>
-      <WishesCreatingPage/>
+      <Routes>
+        <Route path='/' element={<WishesCreatingPage/>}/>
+      </Routes>
     </section>
   )
 }
