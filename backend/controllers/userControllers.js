a4244e118b71d1ad6f5c6c00a5079f9768b108e1
Madan k <partagowda15@gmail.com>
2024-08-07 02:32:05 +0530

    m

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,60 +1,26 @@
 import {errorHandler} from '../utils/errorHandler.js';
 import UserDb from '../modules/userDB.js';
 
-// export const postUsers = async(req,res,next)=>{
-//     try {
-//         const {name,age,message,customeEnd} = req.body;
-
-//         let value;
-//         if(!name || !age || !message){
-//             return next(errorHandler(404,'user input required'));
-//         };
-//         if(customeEnd){
-//             value = customeEnd
-//         }else{
-//             value = Math.floor(100000 + Math.random() * 900000)
-//         }
-//         const userDb =await UserDb.create({name:name,age:age,message:message,customeEnd:value})
-
-//         res.status(200).json({message:"ok",id:userDb._id,name:userDb.name,age:userDb.age,message:userDb.message,customeEnd:userDb.customeEnd});
-//     } catch (error) {
-//         next(error);
-//     }
-// }
-
-export const postUsers = async (req, res, next) => {
+export const postUsers = async(req,res,next)=>{
     try {
-      const { name, age, message, customeEnd } = req.body;
-  
-      if (!name || !age || !message) {
-        return next(errorHandler(400, 'Please provide all required fields: name, age, message'));
-      }
-  
-      const userDb = await UserDb.create({ name, age, message, customeEnd });
-  
-      res.status(200).json({
-        message: "ok",
-        id: userDb._id,
-        name: userDb.name,
-        age: userDb.age,
-        message: userDb.message,
-        customeEnd: userDb.customeEnd,
-      });
-    } catch (error) {
-      if (error.name === 'ValidationError') {
-        const validationErrors = error.errors; 
-        const formattedErrors = {};
-  
-        for (const field in validationErrors) {
-          formattedErrors[field] = validationErrors[field].message;
+        const {name,age,message,customeEnd} = req.body;
+
+        let value;
+        if(!name || !age || !message){
+            return next(errorHandler(404,'user input required'));
+        };
+        if(customeEnd){
+            value = customeEnd
+        }else{
+            value = Math.floor(100000 + Math.random() * 900000)
         }
-  
-        return res.status(400).json({ message: 'Validation error', errors: formattedErrors });
-      }
-      next(error);
+        const userDb =await UserDb.create({name:name,age:age,message:message,customeEnd:value})
+
+        res.status(200).json({message:"ok",id:userDb._id,name:userDb.name,age:userDb.age,message:userDb.message,customeEnd:userDb.customeEnd});
+    } catch (error) {
+        next(error);
     }
-  };
-  
+}
 export const getUser = async(req,res,next)=>{
     try {
         const {id} = req.params;
