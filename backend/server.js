081c577624203c753491fc3e28c7c6acb060f126
Madan k <partagowda15@gmail.com>
2024-08-07 02:00:13 +0530

    remove

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ mongodbConnection();
 
 app.use(cors({origin:'http://localhost:5173',credentials:true}));
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 
 app.use('/',(res,req)=>{
