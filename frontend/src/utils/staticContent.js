0527c0a5891f8c39e53ba4e0eca88f5c5be02eb0
Madan k <partagowda15@gmail.com>
2024-08-08 00:03:15 +0530

    chage url

--- frontend/src/utils/staticContent.js
+++ frontend/src/utils/staticContent.js
@@ -1,5 +1,4 @@
 export const API_URL = 'http://localhost:5173'
 
-export const FRONT_API_URL = 'https://master--beeta-birthday-mewtru.netlify.app';
+export const FRONT_API_URL = 'https://beeta-birthday-mewtru.netlify.app';
 export const SERVER_API_URL = 'https://birthday-mewtru-backend.onrender.com';
-
